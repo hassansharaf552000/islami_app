36656e2cd3a741d1d061b6b86fa520261bfe8de9
hassansharaf552000 <hassansharaf54200022@gmail.com>
2021-11-22 13:24:25 +0200

    finish all app except radio api

--- lib/Home/HomeScreen.dart
+++ lib/Home/HomeScreen.dart
@@ -1,69 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:islami/Home/hadeth/HadethTab.dart';
-import 'package:islami/Home/quran/QuranTab.dart';
-import 'package:islami/Home/radio/RadioTab.dart';
-import 'package:islami/Home/tasbeh/TasbehTab.dart';
-import 'package:islami/main.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:islami_online_c4/home/hadeth/HadethTab.dart';
+import 'package:islami_online_c4/home/quran/QuranTab.dart';
+import 'package:islami_online_c4/home/radio/RadioTab.dart';
+import 'package:islami_online_c4/home/settings/SettingsTab.dart';
+import 'package:islami_online_c4/home/tasbeh/TasbehTab.dart';
+import 'package:islami_online_c4/providers/AppConfigProvider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  static const String routename = 'Home';
+  static const String routeName = 'home';
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int curentindex = 0;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Image.asset('assets/images/main_background.png'),
-      Scaffold(
-        appBar: AppBar(
-          title: Text('islami'),
+    var provider = Provider.of<AppConfigProvider>(context);
+    return Stack(
+      children: [
+        Image.asset(
+          provider.isDarkMode()
+              ? 'assets/images/main_background_dark.png'
+              : 'assets/images/main_background.png',
+          fit: BoxFit.fill,
+          width: double.infinity,
         ),
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-            canvasColor: MyThemeDta.primarycolor,
+        Scaffold(
+          appBar: AppBar(
+            title: Text(AppLocalizations.of(context)!.app_title),
           ),
-          child: BottomNavigationBar(
-            currentIndex: curentindex,
-            onTap: (index) {
-              curentindex = index;
-              setState(() {});
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/moshaf.png')),
-                label: 'Quran',
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/hadeth.png')),
-                label: 'Hadeth',
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/sebha.png')),
-                label: 'Tasbeh',
-              ),
-              BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/radio.png')),
-                label: 'Radio',
-              ),
-            ],
+          bottomNavigationBar: Theme(
+            data: Theme.of(context)
+                .copyWith(canvasColor: Theme.of(context).primaryColor),
+            child: BottomNavigationBar(
+              currentIndex: currentIndex,
+              onTap: (index) {
+                currentIndex = index;
+                setState(() {});
+              },
+              items: [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/ic_quran.png')),
+                    label: AppLocalizations.of(context)!.quran),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/ic_hadeth.png')),
+                    label: AppLocalizations.of(context)!.hadeth),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+                    label: AppLocalizations.of(context)!.tasbeh),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/ic_radio.png')),
+                    label: AppLocalizations.of(context)!.radio),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings),
+                    label: AppLocalizations.of(context)!.settings),
+              ],
+            ),
+          ),
+          body: Container(
+            child: views[currentIndex],
           ),
         ),
-        body: Container(
-          child: views[curentindex],
-        ),
-      ),
-    ]);
+      ],
+    );
   }
 
   List<Widget> views = [
-    QuranTap(),
+    QuranTab(),
     HadethTab(),
     TasbehTab(),
     RadioTab(),
+    SettingsTab()
   ];
 }
--- lib/Home/tasbeh/TasbehTab.dart
+++ lib/Home/tasbeh/TasbehTab.dart
@@ -1,10 +1,99 @@
 import 'package:flutter/material.dart';
+import 'dart:math'as math;
+import '../../main.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class TasbehTab extends StatelessWidget {
+class TasbehTab extends StatefulWidget {
+  @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
+
+class _TasbehTabState extends State<TasbehTab> {
+  int counter=0;
+
+  List<String>tasbehlist=['سبحان الله','الحمد لله','الله اكبر'," لاحول ولاقوه الا بالله"];
+
+  int currentindex=0;
+
+  double angle=0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Center(
+      child: Container(
+        child: Column(
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.topCenter,
+              children: [
+                InkWell(
+                  onTap: ontasbeh_click,
+                  child: Container(
+                    margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.1),
+                    child: Transform.rotate(
+                        angle:math.pi/180* angle,
+                        child: Image.asset('assets/images/body of seb7a.png')),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: MediaQuery.of(context).size.height*0.05),
+                  child: Image.asset('assets/images/head of seb7a.png'),
+                ),
+              ],
+            ),
+            Expanded(child: Column(
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text("عدد التسبيحات",style: TextStyle(fontSize: 25,
+                      fontWeight:FontWeight.w600,),
+                    )),
+                SizedBox(height: 15,),
+                Container(
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    color: MyThemeData.primaryColor,
+                    borderRadius: BorderRadius.circular(15),
+
+
+                  ),
+                  child: Text('$counter',style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+                ),
+                SizedBox(height: 15,),
+                Container(
+                  height: 80,
+                  width: 200,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: MyThemeData.primaryColor,
+                    borderRadius: BorderRadius.circular(20),
+
+
+                  ),
+                  child: Text(tasbehlist[currentindex],style: TextStyle(fontSize: 30,
+                      fontWeight: FontWeight.bold,color: Colors.white),textAlign: TextAlign.center,),
+                )
+              ],
+            )),
+
+          ],
+        ),
+      ),
     );
+  }
+
+  void ontasbeh_click(){
+    setState(() {
+      angle+=30;
+      counter++;
+      if(counter%33==0){
+        if(currentindex==4){
+          currentindex=0;
+        }
+        currentindex++;
+      }
+
+    });
+
   }
 }
